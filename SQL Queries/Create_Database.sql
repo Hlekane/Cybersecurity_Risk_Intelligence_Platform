40e809f0b675723505349e4125da7e060ae79409
Hlekane <pertuniangobeni2@gmail.com>
2026-03-31 20:04:08 +0200

    Create dimensions and Fact table

--- SQL Queries/Create_Database.sql
+++ SQL Queries/Create_Database.sql
@@ -1,87 +1,85 @@
--- Create a database
-
-CREATE DATABASE Cybersecurity_Risk_Intelligence_Platform;
 
 GO
 
-USE Cybersecurity_Risk_Intelligence_Platform;
+USE Cybersecurity_Intelligence_Platform;
 
--- Create tables
-
--- Create the Date Dimension 
-CREATE TABLE Dim_Date (
-
-	Date_id DATE PRIMARY KEY,
-    Year INT,
-    Month INT,
-    Day INT,
-    Weekday VARCHAR(15)
-);
-
--- Create the User Dimension 
-
+-- Create Dimensions
 CREATE TABLE Dim_User (
-    User_ID INT PRIMARY KEY,
-    Full_Name VARCHAR (255),
-    Department VARCHAR(255),
-    Role VARCHAR(255),
-    Location VARCHAR(255),
-    Employment_Type VARCHAR(255)
+    User_ID INT IDENTITY(1,1)PRIMARY KEY,
+    User_Name VARCHAR(100),
+    Email VARCHAR(150),
+    Department VARCHAR(100),
+    Role VARCHAR(100),
+    Location VARCHAR(100),
+    Employment_Type VARCHAR(50),
+    Join_Date DATE,
+    Status VARCHAR(50)
 );
 
--- Create the Event Type Dimension
-
-CREATE TABLE Dim_Event_Type (
-    Event_Type_ID INT PRIMARY KEY IDENTITY(1,1), -- Auto-incriment feild, automatically assign a new number when a new record is inserted
-    Event_Type_Name VARCHAR(255),
-    Category VARCHAR(255),   -- Risk / Training / Awareness
-    Description VARCHAR(255)
+CREATE TABLE Dim_Date (
+    Date_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Day INT,
+    Month INT,
+    Year INT,
+    Month_Name VARCHAR(20),
+    Week INT
 );
-
--- Create the Training Dimension 
 
 CREATE TABLE Dim_Training (
-    Training_ID INT PRIMARY KEY IDENTITY(1,1),
-    Training_Name VARCHAR(255),
-    Difficulty_Level VARCHAR(255),
-    Duration_Minutes INT,
-    Provider VARCHAR(255)
+    Training_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Module_Name VARCHAR(150),
+    Category VARCHAR(100),
+    Difficulty_Level VARCHAR(50),
+    Duration_Minutes INT
 );
 
--- Create the Threat Dimension 
 CREATE TABLE Dim_Threat (
-    Threat_ID INT PRIMARY KEY IDENTITY(1,1),
-    Threat_Type VARCHAR(255),
-    Severity_Level VARCHAR(255),
-    Description VARCHAR(255)
+    Threat_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Threat_Type VARCHAR(100),
+    Severity VARCHAR(50),
+    Description TEXT
 );
 
--- Create the Fact Table
+
+CREATE TABLE Dim_Event_Type (
+    Event_Type_ID INT IDENTITY(1,1)PRIMARY KEY,
+    Event_Name VARCHAR(100),
+    Event_Category VARCHAR(100)
+   
+);
+
+-- Create a Fact Table
 
 CREATE TABLE Fact_User_Cyber_Event (
-    event_id INT PRIMARY KEY,
-    user_id INT,
-    date_id DATE,
-    event_type_id INT,
-    training_id INT NULL,
-    threat_id INT NULL,
-    device_type VARCHAR(50),
-    risk_score INT,
+    Event_ID INT IDENTITY(1,1)PRIMARY KEY,
 
-    -- Use constraint to prevent actions that could destroy the link between tables
+    User_ID INT,
+    Date_ID INT,
+    Training_ID INT,
+    Threat_ID INT,
+    Event_Type_ID INT,
 
+    Quiz_Score DECIMAL(5,2),
+    Passed_Flag BIT,
+    Clicked_Flag BIT,
+    Reported_Flag BIT,
+    Risk_Weight DECIMAL(5,2),
+
+    -- Foreign Keys
     CONSTRAINT FK_User
-        FOREIGN KEY (user_id) REFERENCES Dim_User(user_id),
+        FOREIGN KEY (User_ID) REFERENCES Dim_User(User_ID),
 
     CONSTRAINT FK_Date
-        FOREIGN KEY (date_id) REFERENCES Dim_Date(date_id),
-
-    CONSTRAINT FK_EventType
-        FOREIGN KEY (event_type_id) REFERENCES Dim_Event_Type(event_type_id),
+        FOREIGN KEY (Date_ID) REFERENCES Dim_Date(Date_ID),
 
     CONSTRAINT FK_Training
-        FOREIGN KEY (training_id) REFERENCES Dim_Training(training_id),
+        FOREIGN KEY (Training_ID) REFERENCES Dim_Training(Training_ID),
 
     CONSTRAINT FK_Threat
-        FOREIGN KEY (threat_id) REFERENCES Dim_Threat(threat_id)
+        FOREIGN KEY (Threat_ID) REFERENCES Dim_Threat(Threat_ID),
+
+    CONSTRAINT FK_Event_Type
+        FOREIGN KEY (Event_Type_ID) REFERENCES Dim_Event_Type(Event_Type_ID)
 );
+
+
